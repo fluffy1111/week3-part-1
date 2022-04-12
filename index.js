4652f52e04fde356ab9cba8750d20a226bf053b5
fluffy1111 <71845250+fluffy1111@users.noreply.github.com>
2022-04-11 23:39:08 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var age = [ "a", "b", "c", "d", "e", "f", "g", "h"]
+var a=3
+var b=9
+var c=23
+var d=64
+var e=2
+var f=8
+var g=28
+var h=93
+
+do {
+    age = age - age;
+    result = result -i;
+} while (i < 7);
+
+console.log(result)
